4ecf7a1ab4bdc7c9f43e17a8f6387037bb76f4f7
Josh.ep <90182193+joshchan1301@users.noreply.github.com>
2026-04-09 01:43:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ async def chat_with_ai(req: ChatRequest):
         return {"reply": "Lỗi: Server chưa cấu hình API Key miễn phí."}
         
     # URL API của Google Gemini
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
+    url = f"https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
     
     # Cấu hình Prompt chuyên gia y tế
     system_instruction = (
